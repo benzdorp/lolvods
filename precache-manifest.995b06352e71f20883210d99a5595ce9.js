421798317232fe174b5140e0b71129162e52a8ad
Zach Wolfe <wolfe.zach@gmail.com>
2019-03-17 00:57:43 -0700

    Updates

---
+++ precache-manifest.995b06352e71f20883210d99a5595ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abc40a197b6dc2a7c20f",
+    "url": "/cascade/static/js/runtime~main.abc40a19.js"
+  },
+  {
+    "revision": "9d25457facd48e1d3819",
+    "url": "/cascade/static/js/main.9d25457f.chunk.js"
+  },
+  {
+    "revision": "9bae506e6db10feaa672",
+    "url": "/cascade/static/js/1.9bae506e.chunk.js"
+  },
+  {
+    "revision": "9d25457facd48e1d3819",
+    "url": "/cascade/static/css/main.cea5c660.chunk.css"
+  },
+  {
+    "revision": "9bae506e6db10feaa672",
+    "url": "/cascade/static/css/1.80b53a3b.chunk.css"
+  },
+  {
+    "revision": "b4d079fc25ced18125ed5e3acebf07ea",
+    "url": "/cascade/index.html"
+  }
+];
